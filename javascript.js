dd103160d33445fef0622bef770b1ca517790ed9
Sagar0298 <188861999+Sagar0298@users.noreply.github.com>
2025-08-08 15:40:54 +0530

    Created function playRound.

--- javascript.js
+++ javascript.js
@@ -23,7 +23,39 @@ const humanScore = 0;
 const computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == Rock && computerChoice == Rock) {
-
+    if (humanChoice == "Rock" && computerChoice == "Rock") {
+        ++humanScore;
+        ++computerScore;
+        return console.log("it's a draw.");
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        ++computerChoice;
+        return console.log("Computer win, Paper beat rock");
+    } else if (humanChoice == "Rock" && computerChoice == "Scissor") {
+        ++humanScore;
+        return console.log("human win, rock beats scissor");
+    } else if (humanChoice == "paper" && computerChoice == "Rock") {
+        ++humanScore;
+        return console.log("human win, paper beats rock");
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        ++humanScore;
+        ++computerScore;
+        return console.log("it's a draw.");
+    } else if (humanChoice == "paper" && computerChoice == "scissor") {
+        ++computerChoice;
+        return console.log("Computer win, scissor beats paper");
+    } else if (humanChoice == "scissor" && computerChoice == "Rock") {
+        ++computerChoice;
+        return console.log("Computer win, rock beats scissor");
+    } else if (humanChoice == "scissor" && computerChoice == "paper") {
+        ++humanScore;
+        return console.log("human win, scissor beats paper");
+    } else if (humanChoice == "scissor" && computerChoice == "scissor") {
+        ++humanScore;
+        ++computerScore;
+        return console.log("it's a draw.");
     }
+
 }
+console.log(playRound())
+console.log(humanScore)
+console.log(computerScore)
