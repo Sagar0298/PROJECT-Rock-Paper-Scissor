980472486e30d57b3f78a6a0b900dfec22a1005d
Sagar0298 <188861999+Sagar0298@users.noreply.github.com>
2025-08-08 16:47:40 +0530

    fix many bugs. i know it's not a good message.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 "use strict"
 console.log("Hello World")
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 100);
+    let randomNumber = Math.floor(Math.random() * 100);
     if (randomNumber > 60) {
         return 'Rock'
     } else if (randomNumber < 30) {
@@ -13,49 +13,49 @@ function getComputerChoice() {
 
 console.log(getComputerChoice())
 
-const humanChoice = prompt("Chose One: Rock, paper, Scissor")
+let humanChoice = prompt("Chose One: Rock, paper, Scissor")
 function getHumanChoice() {
     return humanChoice;
 }
 console.log(getHumanChoice())
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "Rock" && computerChoice == "Rock") {
         ++humanScore;
         ++computerScore;
-        return console.log("it's a draw.");
+        console.log("it's a draw.");
     } else if (humanChoice == "rock" && computerChoice == "paper") {
-        ++computerChoice;
-        return console.log("Computer win, Paper beat rock");
+        ++computerScore;
+        console.log("Computer win, Paper beat rock");
     } else if (humanChoice == "Rock" && computerChoice == "Scissor") {
         ++humanScore;
-        return console.log("human win, rock beats scissor");
+        console.log("human win, rock beats scissor");
     } else if (humanChoice == "paper" && computerChoice == "Rock") {
         ++humanScore;
-        return console.log("human win, paper beats rock");
+        console.log("human win, paper beats rock");
     } else if (humanChoice == "paper" && computerChoice == "paper") {
         ++humanScore;
         ++computerScore;
-        return console.log("it's a draw.");
+        console.log("it's a draw.");
     } else if (humanChoice == "paper" && computerChoice == "scissor") {
-        ++computerChoice;
-        return console.log("Computer win, scissor beats paper");
+        ++computerScore;
+        console.log("Computer win, scissor beats paper");
     } else if (humanChoice == "scissor" && computerChoice == "Rock") {
-        ++computerChoice;
-        return console.log("Computer win, rock beats scissor");
+        ++computerScore;
+        console.log("Computer win, rock beats scissor");
     } else if (humanChoice == "scissor" && computerChoice == "paper") {
         ++humanScore;
-        return console.log("human win, scissor beats paper");
+        console.log("human win, scissor beats paper");
     } else if (humanChoice == "scissor" && computerChoice == "scissor") {
         ++humanScore;
         ++computerScore;
-        return console.log("it's a draw.");
+        console.log("it's a draw.");
     }
 
 }
-console.log(playRound())
+console.log(playRound(getHumanChoice(), getComputerChoice()))
 console.log(humanScore)
 console.log(computerScore)
